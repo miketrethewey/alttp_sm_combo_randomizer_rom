af21dbb70be04abb6bc44b0b9b26a92a0ce97580
Mike A. Trethewey <miketrethewey@gmail.com>
2018-11-04 22:17:36 -0800

    Verbose build script

--- build.bat
+++ build.bat
@@ -1,9 +1,20 @@
 @echo off
-echo Building Super Metroid + Zelda
+
+echo Building Super Metroid + A Link to the Past
 cd resources
+
+echo Combining originals (M3:3mb + Z3:1mb) into ExHiROM (Z3M3:6mb)
 python create_exhirom.py sm_orig.sfc alttp_orig.sfc zsm_orig.sfc
+
+echo Creating working copy
 copy /y zsm_orig.sfc zsm.sfc > NUL:
+
+echo Patching working copy
 asar ..\src\main.asm zsm.sfc
+
+echo Copy working copy to \build\
+if not exist "..\build\" mkdir "..\build\"
 copy zsm.sfc "..\build\zsm.sfc" > NUL:
+
 cd ..
 echo Done
